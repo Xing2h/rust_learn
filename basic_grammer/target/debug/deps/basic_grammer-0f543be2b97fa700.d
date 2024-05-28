a59b8ce3997eeb4aeb6c7fc28821888cf399a731
Your Name <your_email@example.com>
2024-05-28 13:27:32 +0800

    iniy

---
+++ basic_grammer/target/debug/deps/basic_grammer-0f543be2b97fa700.d
@@ -0,0 +1,5 @@
+/home/kali/Desktop/rust_learn/basic_grammer/target/debug/deps/libbasic_grammer-0f543be2b97fa700.rmeta: src/main.rs
+
+/home/kali/Desktop/rust_learn/basic_grammer/target/debug/deps/basic_grammer-0f543be2b97fa700.d: src/main.rs
+
+src/main.rs:
---
+++ basic_grammer/target/debug/deps/basic_grammer-a04034bc4e15dcb9.d
@@ -0,0 +1,5 @@
+/home/kali/Desktop/rust_learn/basic_grammer/target/debug/deps/libbasic_grammer-a04034bc4e15dcb9.rmeta: src/main.rs
+
+/home/kali/Desktop/rust_learn/basic_grammer/target/debug/deps/basic_grammer-a04034bc4e15dcb9.d: src/main.rs
+
+src/main.rs:
